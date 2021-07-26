6a01966d7774afc4e62d935286dd983cf9f1893b
xeusteerapat <xeus085@gmail.com>
2021-07-26 09:22:50 +0700

    [ADD] :sparkles: add auth service

---
+++ src/users/auth.service.ts
@@ -0,0 +1,32 @@
+import { UsersService } from './users.service';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    const users = await this.userService.find(email);
+
+    if (users.length) {
+      throw new BadRequestException('Email already in used');
+    }
+
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    const result = salt + '.' + hash.toString('hex');
+
+    const user = await this.userService.create(email, result);
+
+    return user;
+  }
+
+  signin() {
+    console.log('');
+  }
+}
